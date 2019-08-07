989df3fe3196ed3e001debef00b4ecb94168b979
pranithashravani <masamsetty.pranithashravani@gmail.com>
2019-08-07 13:41:27 +0530

    spring boot task using seed-data

---
+++ src/main/java/com/stackroute/springboot/seeddata/SpringContextListner.java
@@ -0,0 +1,12 @@
+package com.stackroute.springboot.seeddata;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringContextListner implements ApplicationListener<ContextRefreshedEvent> {
+  public void onApplicationEvent(ContextRefreshedEvent event) {
+    System.out.println("Started with Applicationlistner");
+  };
+}

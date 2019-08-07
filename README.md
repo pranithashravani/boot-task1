7965826fe9752ce4862bfc120d628b707bb23db3
pranithashravani <masamsetty.pranithashravani@gmail.com>
2019-08-07 12:44:02 +0530

    spring boot task using exceptions

---
+++ README.md
@@ -0,0 +1,6 @@
+# boot-task1
+Tasks to be Done Post Demo
+
+Complete all the endpoints for CRUD operations on Muzix
+
+Create custom exceptions TrackNotFoundException, TrackAlreadyExistsException in a com.stack....exceptions package. Perform appropriate exception handling and propagation Back.
---
+++ src/main/java/com/stackroute/springboot/service/TrackService.java
@@ -0,0 +1,15 @@
+package com.stackroute.springboot.service;
+import com.stackroute.springboot.domain.Track;
+import com.stackroute.springboot.exceptions.TrackAlreadyExists;
+import com.stackroute.springboot.exceptions.TrackNotFound;
+
+import java.util.List;
+
+public interface TrackService {
+    public Track saveTrack(Track track) throws TrackAlreadyExists;
+    public Track deleteTrack(int id) throws TrackNotFound;
+    public  List<Track> getAllTracks() throws TrackNotFound;
+    public Track getTrackById(int id) throws TrackNotFound;
+    public Track UpdateTrack(Track track) throws TrackAlreadyExists;
+
+}
